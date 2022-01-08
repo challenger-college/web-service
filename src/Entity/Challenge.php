30386f5b05ac238c8f41d9b2d7cadaa5b74d5337
mediashare <mediashare.supp@gmail.com>
2022-01-08 21:23:30 +0100

    Big init

---
+++ src/Entity/Challenge.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChallengeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChallengeRepository::class)]
+class Challenge
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
